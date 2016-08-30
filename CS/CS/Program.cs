935b29906da05d8219f260e62adfe6d1f49d592e
K348 <karoldobkowski3@gmail.com>
2016-08-30 06:35:45 +0200

    initial project version

--- CS/CS/Program.cs
+++ CS/CS/Program.cs
@@ -10,6 +10,22 @@ namespace CS
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("First app using C#!");
+            
+            int first_variable_to_add,
+                second_variable_to_add;
+
+            int result_of_adding = 0;
+
+            Console.WriteLine("Type first number");
+            first_variable_to_add = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type second number");
+            second_variable_to_add = Convert.ToInt32(Console.ReadLine());
+
+            result_of_adding = first_variable_to_add + second_variable_to_add;
+            Console.WriteLine("Wynik");
+            Console.Write(result_of_adding);
+            while (true) ;
         }
     }
 }
